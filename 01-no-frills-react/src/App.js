fc5608e92099ff2040f4e1f178f0a2ca19dd35cc
nasifmehdi <91786922+nasifmehdi@users.noreply.github.com>
2023-03-18 20:48:07 +0600

    react hello world

--- 01-no-frills-react/src/App.js
+++ 01-no-frills-react/src/App.js
@@ -1,28 +1,11 @@
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+const App = () =>{
+  return React.createElement('div',{id:'myid', className:'myclass'},
+  React.createElement('h1',{},'Hello React!'
+  ))
+}
 
-const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
-  ]);
-};
+const root = document.getElementById('root')
+const React_DOM = ReactDOM.createRoot(root)
+const React_Function=React.createElement(App)
+React_DOM.render(React_Function)
 
-const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
-root.render(React.createElement(App));
